9bd589a3976d137e0b753e0800532a0711ce1ee0
hjoshuashin <hjoshuashin@gmail.com>
2023-09-30 14:21:06 -0500

    updating

--- Assets/ShelfButtons.cs
+++ Assets/ShelfButtons.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class ShelfButtons : MonoBehaviour
 {
+    public float zoom;
+    private float originalZoom;
+    private float targetZoom = 1f;
+    private float zoomMultiplier = 3000f;
+    private float velocity = 5f;
+    private float smoothTime = 0.25f;
 
-    void ZoomIn()
+    private Camera cam;
+
+    private void Start()
+    { 
+        cam = Camera.main;
+        zoom = cam.orthographicSize;
+        originalZoom = cam.orthographicSize;
+    }
+    public void ZoomIn()
     {
-
+        StartCoroutine(ZoomInEnum());
     }
 
-    void ZoomOut()
+    IEnumerator ZoomInEnum()
+    {
+        while (zoom > targetZoom)
+        {
+            cam.orthographicSize = Mathf.Lerp(originalZoom, targetZoom, 1f);
+            yield return null;
+        }
+    }
+
+    public void ZoomOut()
     {
 
     }
